2fa2f13e2f63e3303811eb1df7221cb39f1f500a
Alexander Apostolov <alexanderapostolov@protonmail.com>
2017-05-30 23:00:54 -0400

    Init

--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+// Import react router v4 with properties to
+// interact with the History library to look at the entire URL,
+// the component that provides the configuration to know what component to show,
+import {BrowserRouter, Route, } from 'react-router-dom';
 
-import App from './components/app';
 import reducers from './reducers';
+import PostsIndex from './components/posts_index';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostsIndex} />
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
