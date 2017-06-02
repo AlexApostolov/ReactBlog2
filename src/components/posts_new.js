129ef58ad5cfffa0fda6a113f4b91bd76764c2c9
Alexander Apostolov <alexanderapostolov@protonmail.com>
2017-06-01 21:19:27 -0400

    Add navigation from form and hook up redux-form.

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,12 +1,36 @@
 import React, {Component} from 'react';
+// reduxForm is very similiar to the "connect" helper from redux
+import {Field, reduxForm} from 'redux-form';
 
 class PostsNew extends Component {
-  render() {
+  // JSX to represent the actual element that gets rendered to the screen
+  // The field.input is an object with different event handlers (onChange/onBlur etc. and the value) and props
+  renderTitleField(field) {
     return (
       <div>
-        PostsNew!
+        <input
+          type="text"
+          {...field.input}
+        />
       </div>
     );
   }
+
+  render() {
+    // Field component is used to interact with Redux Form, but doesn't know how to appear
+    // Pass JSX to "component" prop to handle that.
+    return (
+      <form>
+        <Field
+          name="title"
+          component={this.renderTitleField}
+        />
+      </form>
+    );
+  }
 }
-export default PostsNew;
+export default reduxForm({
+  // Assign form property a unique string name for a component,
+  // can have multiple forms on the screen each with unique string names
+  form: 'PostsNewForm'
+})(PostsNew);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
+import {reducer as formReducer} from 'redux-form';
 import PostsReducer from './reducer_posts';
 
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  // Must be saved as "form" piece of state for all the different forms inside of components (they assume this is the name)
+  form: formReducer
 });
 
 export default rootReducer;
