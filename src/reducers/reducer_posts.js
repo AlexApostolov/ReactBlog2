139759961c107b38c110893be1fd8799b40e9586
Alexander Apostolov <alexanderapostolov@protonmail.com>
2017-06-06 00:22:42 -0400

    Include delete post ability and also clear deleted post from local memory.

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,13 @@
 import _ from 'lodash';
-import {FETCH_POSTS, FETCH_POST} from '../actions';
+import {FETCH_POSTS, FETCH_POST, DELETE_POST} from '../actions';
 
 export default function(state = {}, action) {
   switch (action.type) {
+    // Delete the post in local memory as well and get a snappier UI
+    case DELETE_POST:
+      // Look at the state object, if the state object has a key of the post id, then just omit/drop it
+      // and return a new object without that id.
+      return _.omit(state, action.payload);
     // Make sure the post we want to show is actually contained in state.
     // If you went to the posts/:id route directly, by typing the url in the browser,
     // and skipped going to the index route, there would not be any posts in state,
